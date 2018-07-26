b7ca7819d85cd28247da2ea13a7c761c44a5d017
gtx <673800638@qq.com>
2018-07-26 16:52:49 +0800

    自定义指令，点击空白处关闭弹出菜单

--- vue/src/components/Home.vue
+++ vue/src/components/Home.vue
@@ -25,13 +25,14 @@
                 <p class="level-item has-text-centered">
                     <a class="link is-info">Home</a>
                 </p>
-                <p class="level-item has-text-centered" @click="menu">
-                    <a class="link is-info">Menu</a>
+                <p class="level-item has-text-centered" v-clickOther="clickOther" @click="menu">
+                    <a class="link is-info" id="favorite">Menu</a>
                     <div v-show="menuActive" class="favorite-menu">
                         <span>添加</span>
                         <ul>
                             <li>123123123</li>
                             <li>12313231</li>
+                            <li>123fdafdasfdasfa</li>
                             <li>123fdafdasfdasfa</li>
                         </ul>
 
@@ -54,35 +55,35 @@
             </nav>
         </div>
 
-        <div class="box">
-            <div class="nav img">
-
-            </div>
-            <nav class="level">
-                <p class="level-item has-text-centered">
-                    <a class="link is-info">Home</a>
-                </p>
-                <p class="level-item has-text-centered" @click="menu">
-                    <a class="link is-info">Menu</a>
-                    <ul v-show="menuActive" class="favorite-menu">
-                        <li>123123123</li>
-                        <li>12313231</li>
-                        <li>123fdafdasfdasfa</li>
-                    </ul>
-                </p>
-                <p class="level-item has-text-centered">
-                    <a class="link is-info">Reservations</a>
-                </p>
-                <p class="level-item has-text-centered">
-                    <a class="link is-info">Contact</a>
-                </p>
-            </nav>
-        </div>
-
+        <!-- <div class="mask"></div> -->
     </div>
 </template>
 
 <script>
+const clickOther = {
+    bind: function(el, binding, vnode) {
+        function documentHandler(e) {
+            // 这里判断点击的元素是否是本身，是本身，则返回
+            if (el.contains(e.target)) {
+                return false;
+            }
+            // 判断指令中是否绑定了函数
+            if (binding.expression) {
+                // 如果绑定了函数 则调用那个函数，此处binding.value就是handleClose方法
+                binding.value(e);
+            }
+        }
+        // 给当前元素绑定个私有变量，方便在unbind中可以解除事件监听
+        el.__vueClickOutside__ = documentHandler;
+        document.addEventListener('click', documentHandler);       
+    },
+    update() {},
+    unbind(el, binding) {
+        // 解除事件监听
+        document.removeEventListener('click', el.__vueClickOutside__);
+        delete el.__vueClickOutside__;
+    },    
+};
 export default {
     data() {
         return {
@@ -91,6 +92,7 @@ export default {
             menuActive: false
         };
     },
+    directives: { clickOther },
     computed: {
         token: function() {
             return localStorage.getItem("access_token");
@@ -134,6 +136,10 @@ export default {
         menu: function() {
             this.menuActive = !this.menuActive;
             console.log(this.menuActive);
+        },
+        clickOther: function() {
+            this.menuActive = false
+            console.log('clickOther:'+this.menuActive)
         }
     }
 };
@@ -146,30 +152,46 @@ export default {
     left: 35%;
     width: 18rem;
     background-color: #fff;
-    border: 1px solid #eceeef;
+    border: 1px solid #fff;
     box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1);
     -webkit-transform: translate(-50%);
     transform: translate(-50%);
-    z-index: 10;
+    z-index: 20;
 }
 
 .favorite-menu {
+    padding: 0.7rem;
     text-align: left;
 }
 
 .favorite-menu li {
-    border-bottom: solid 1px red;
+    border-bottom: solid 1px #eee;
+    padding: 0.3rem;
 }
 .favorite-menu li:last-child {
     border-bottom-style: none;
 }
 
 .nav {
-    height: 800px;
+    height: 100px;
     background-color: #eee;
 }
 
 nav {
     position: relative;
+}
+
+/* 遮罩层 */
+.mask {
+    background: rgba(0, 0, 0, 0);
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    display: none;
+    z-index: 20;
+    filter: alpha(opacity=60);
+    opacity: 0.5 !important;
 }
 </style>
